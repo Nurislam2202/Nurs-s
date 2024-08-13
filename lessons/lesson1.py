cdf846fb96d14ca30843a9d9a310dd8a786e13af
Nurislam2202 <nurislamuraimov2202@gmail.com>
2024-08-13 23:48:37 +0600

    lesson1

---
+++ lessons/lesson1.py
@@ -0,0 +1,49 @@
+# # классы, git
+# # ООП - обьектно ориентированное программирование
+# # все в python(-е) это - ОБЬЕКТЫ
+#
+# tupleVariable1 = 1,True,'10',1.1,{},[],(),None
+# tupleVariable2 = 1,1,'ertyu'
+# print(type(tupleVariable1))
+#
+# stringVariable = 'etyu'
+# # print(stringVariable.)
+#
+#
+# def a(name):
+#     print('привет',name)
+#
+# a('Атабек')
+#
+#
+# class Person:
+#
+#     # метод - функция 'def'
+#     def nameprint(self):
+#         print('мое имя:',self.name)
+#
+#     # магический метод
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+#     def __str__(self):
+#         # return 'привет'
+#         return f'{self.name} {self.age}'
+#
+#     def __len__(self):
+#         return len(self.name)
+#
+#
+# # обьект\экземпляр
+# atabek = Person('Атабек', 21)
+# islam = Person('Ислам', 22)
+#
+# print(atabek)
+# print(len(atabek))
+#
+# atabek.nameprint()
+# islam.nameprint()
+# Person.nameprint(atabek)
+#
+# print(type(atabek))
